5b552897d5984f63e2ff29b60843b583b9911027
RyUKEnza <kenza-raj@hotmail.fr>
2023-09-22 16:57:48 -0700

    fix problem

--- printf.c
+++ printf.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+
 /**
  * _printf - Produces output according to a format.
  * @format: A character string.
@@ -11,81 +12,100 @@
  */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int count = 0;
-        char c;
+	va_list args;
+	int count = 0;
+	char c;
 
-        va_start(args, format);
 
-        while (*format)
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        if (*format == 'c')
-                        {
-                                c = va_arg(args, int);
-                                write(1, &c, 1);
-                                count++;
-                        }
-                        else if (*format == 's')
-                        {
-                                char *str = va_arg(args, char *);
+	va_start(args, format);
 
-                                if (str == NULL)
-                                        str = "(null)";
-                                while (*str)
-                                {
-                                        write(1, str, 1);
-                                        str++;
-                                        count++;
-                                }
-                        }
-                        else if (*format == 'd' || *format == 'i')
-                        {
-                                int num = va_arg(args, int);
-                                int digits = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+
+
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					write(1, str, 1);
+					str++;
+					count++;
+				}
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+				int digits = 0;
 				int temp = num;
-                                char *num_str;
-                                int i;
+				char *num_str;
+				int i;
 
-                                if (num < 0)
-                                {
-                                        write(1, "-", 1);
-                                        count++;
-                                        temp = -temp;
-                                }
 
-                                while (temp != 0)
-                                {
-                                        temp /= 10;
-                                        digits++;
-                        }
+				if (num < 0)
+				{
+					write(1, "-", 1);
+					count++;
+					temp = -temp;
+				}
 
-                                num_str = malloc((digits + 1) * sizeof(char));
 
-                                for ( i = digits - 1; i >= 0; i--)
-                                {
-                                        num_str[i] = (num % 10) + '0';
-                                        num /= 10;
-                                }
+				while (temp != 0)
+				{
+					temp /= 10;
+					digits++;
+				}
 
-                                write(1, num_str, digits);
-                                count += digits;
 
-                                free(num_str);
-                        }
-                }
-                else
-                {
-                        write(1, format, 1);
-                        count++;
-                }
+				num_str = malloc((digits +1) * sizeof(char));
 
-                format++;
-        }
 
-        va_end(args);
+				for ( i = digits - 1; i >= 0; i--)
+				{
+					num_str[i] = (num % 10) + '0';
+					num /= 10;
+				}
 
-        return (count);
+
+				write(1, num_str, digits);
+				count += digits;
+
+
+				free(num_str);
+			}
+			else if (*format == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			count++;
+
+
+		}
+
+
+		format++;
+	}
+
+
+	va_end(args);
+
+
+	return (count);
 }
